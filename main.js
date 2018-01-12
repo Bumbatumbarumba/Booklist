0335b86e3fe9391dab79b8598c611647b4f6ff27
Bartosz Kosakowski <kosakowb@mcmaster.ca>
2018-01-11 23:41:45 -0500

    I spent like 4 hours trying to read and write data to a file with js, idgaf about proper commits at this point

--- main.js
+++ main.js
@@ -7,20 +7,40 @@ list of books on the front page
 
 /*
 */
-function submitForm(){
-	var textboxInput = document.getElementById("addbook");
-	var someText = "";
-	for (var i = 0; i < textboxInput.length; i++){
-		someText += textboxInput[i].value;
-	}
-	window.alert(someText);
-	
+var fs = require("fs");
 
+function submitForm(){
+	const textboxInput = document.getElementById("addbook");
+	var rating = "";
+	for (var i = 0; i < textboxInput.length; i++){
+		if (textboxInput[i].checked == true){
+			rating = textboxInput[i].value;
+		}
+	}
+
+	/*var addBookParams = JSON.stringify({"title": textboxInput[0].value, 
+		"author": textboxInput[1].value, 
+		"rating": rating, 
+		"comments":textboxInput[8].value});
+	window.alert(addBookParams);
+	window.alert(JSON.parse(addBookParams).title);*/
+
+	/*var newBook = new bookData(textboxInput[0].value, textboxInput[1].value, rating, textboxInput[8].value);
+	window.alert(JSON.stringify(newBook));
 	resetForm();
-	window.alert("Book added to list!");
+	window.alert("Book added to list!");*/
 }
 
 //used to reset the add book form once the user submits their info
 function resetForm(){
 	document.getElementById("addbook").reset();
 }
+/*
+class bookData{
+	constructor(title, author, rating, comment){
+		this.title = title;
+		this.author = author;
+		this.rating = rating;
+		this.comment = comment;
+	}
+}*/
